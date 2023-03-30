bc9b563af8231cf7c78161f2e83c790e4084cfc8
ElLord0214 <elvaderst08@gmail.com>
2023-03-29 22:22:57 -0400

    Create Tarea 3.py
    Hola

---
+++ Tarea 3.py
@@ -0,0 +1,8 @@
+def main():
+    num1 = int(input("Ingrese el primer número: "))
+    num2 = int(input("Ingrese el segundo número: "))
+    resultado = num1 + num2
+    print("El resultado de la suma es:", resultado)
+
+if __name__ == "__main__":
+    main()
