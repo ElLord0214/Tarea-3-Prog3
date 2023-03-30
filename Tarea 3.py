4146e0dde98e521336b7f8c933710e18e9b39d6e
ElLord0214 <elvaderst08@gmail.com>
2023-03-29 22:42:18 -0400

    Update Tarea 3.py
    holA

--- Tarea 3.py
+++ Tarea 3.py
@@ -2,7 +2,8 @@ def main():
     num1 = int(input("Ingrese el primer número: "))
     num2 = int(input("Ingrese el segundo número: "))
     resultado = num1 + num2
+    resultado = num1 - num2
     print("El resultado de la suma es:", resultado)
-
+    print("El resultado de la resta es:", resultado)
 if __name__ == "__main__":
     main()
